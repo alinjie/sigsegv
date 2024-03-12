a040ac9f9fa1e3ef2a20fc3b70b1f1ec844faf69
Ali Kristoffer Njie <ali.k.njie@gmail.com>
2024-03-12 21:30:04 +0100

    works again

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,6 @@
-import blurhash from "@mux/blurhash"
 import type { ServerLoad } from "@sveltejs/kit"
 
 export const load: ServerLoad = async () => {
   const muxPlaybackId = "svvqOj01YsD02Hkhd0267CAB1B02GoeL4eS2TsJ4cN1vlwk"
-  return await blurhash(muxPlaybackId)
+  return { blurhash: "invalid" }
 }
